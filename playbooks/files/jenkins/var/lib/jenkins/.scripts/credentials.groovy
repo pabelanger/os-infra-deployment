68fc704ffd4b85ced57255721a1b9d1b612bc733
Paul Belanger <pabelanger@redhat.com>
2015-11-30 15:14:50 -0500

    More changes
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

---
+++ playbooks/files/jenkins/var/lib/jenkins/.scripts/credentials.groovy
@@ -0,0 +1,19 @@
+import jenkins.model.Jenkins;
+import com.cloudbees.plugins.credentials.domains.Domain;
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey;
+
+domain = Domain.global()
+credentials_store =
+    Jenkins.instance.getExtensionList(
+        'com.cloudbees.plugins.credentials.SystemCredentialsProvider'
+    )[0].getStore()
+credentials = new BasicSSHUserPrivateKey(
+    CredentialsScope.GLOBAL,
+    "9292f6a1-ebc6-40e8-80da-b8940261cdce",
+    "jenkins",
+    new BasicSSHUserPrivateKey.UsersPrivateKeySource(),
+    "",
+    "<!-- Managed by Ansible -->"
+)
+credentials_store.addCredentials(domain, credentials)
